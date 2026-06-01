457f9f46f80d8f8c5458a802d18088cdefa72c7c
Rui <ruiteruakirts@gmail.com>
2026-06-01 20:16:16 -0300

    Teste php

---
+++ SistemaCompras/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema de Pedidos</title>
+</head>
+<body>
+    <? include("./StartupModelSenior/SistemaCompras/layout/menu.php") ?>
+    <p>Teste</p>
+</body>
+</html>
---
+++ SistemaCompras/layout/menu.php
@@ -0,0 +1,13 @@
+<link rel="stylesheet" href="./style.css">
+<nav id="sidebar" style="display:none;">
+    <h2>GESTOR PRO</h2>
+    <ul>
+        <li onclick="showScreen('cadastro-categoria')">Cadastro de Categorias</li>
+        <li onclick="showScreen('cadastro-itens')">Cadastro de Itens</li>
+        <li onclick="showScreen('cadastro-usuarios')">Cadastro de Usuários</li>
+        <li onclick="showScreen('selecao-itens')">Solicitar Itens</li>
+        <li onclick="showScreen('gestao')">Gestão e Avaliação</li>
+        <li onclick="showScreen('relatorio')">Relatórios</li>
+        <li onclick="location.reload()">Sair</li>
+    </ul>
+</nav>
